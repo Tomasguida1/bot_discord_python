ca1bfdc655b1ca12e78572bd655293c3d0a2f681
Tomas Guida <tomasguida2000@hotmail.com>
2023-03-04 15:20:39 -0300

    sum and status commands added

--- src/index.py
+++ src/index.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import datetime
 
 #initializing bot
 intents = discord.Intents.all()
@@ -8,8 +9,21 @@ bot = commands.Bot(command_prefix='>', description="my bot",intents=intents)
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send("pong")
-    
+    await ctx.send("pov")
 
-bot.run(TOKEN)
+@bot.command()
+async def sum(ctx, n1:int, n2:int):
+    await ctx.send(n1 + n2)
+
+@bot.command()
+async def stats (ctx):
+    embed = discord.Embed(title =f"{ctx.guild.name}", description = "hola soy el gato de Miga", timestamp=datetime.datetime.utcnow())
+    await ctx.send(embed=embed)
+    
+#event
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name=">commands"))
+    print ("my bot is ready")
+bot.run(token)
     
