2567c02fc7a142676a54fa88cf6e61f6e68bd5a0
Tomas Guida <tomasguida2000@hotmail.com>
2023-03-04 14:48:33 -0300

    now working FIX

--- src/index.py
+++ src/index.py
@@ -2,12 +2,14 @@ import discord
 from discord.ext import commands
 
 #initializing bot
-bot = commands.Bot(command_prefix='>', description="my bot")
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='>', description="my bot",intents=intents)
 
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
     
 
-bot.run("MTA4MTYxODg3ODYwMzQ2NDcyNA.GcIJ5T.3A-r9HZDPdUXF0wQ3SdAZL-HX0mkTVaxGMHhfI")
+bot.run(TOKEN)
     
