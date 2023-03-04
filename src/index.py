a50174be0cc9f47c1c81fa30791dd42a53df9b1f
Tomas Guida <tomasguida2000@hotmail.com>
2023-03-04 14:31:13 -0300

    now working

--- src/index.py
+++ src/index.py
@@ -3,3 +3,11 @@ from discord.ext import commands
 
 #initializing bot
 bot = commands.Bot(command_prefix='>', description="my bot")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+    
+
+bot.run("MTA4MTYxODg3ODYwMzQ2NDcyNA.GcIJ5T.3A-r9HZDPdUXF0wQ3SdAZL-HX0mkTVaxGMHhfI")
+    
